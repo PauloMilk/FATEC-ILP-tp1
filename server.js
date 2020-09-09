0a959805e7e6c0e17fdfc8c770272db0d6bd72e5
Paulo Leite Costa <43909062+PauloMilk@users.noreply.github.com>
2020-09-08 21:36:29 -0300

    Atualizando config de porta para o heroku

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(express.static('public'));
 app.use('/css', express.static(__dirname+'/node_modules/materialize-css/dist/css'))
 app.use('/js', express.static(__dirname+'/node_modules/materialize-css/dist/js'))
 
-app.listen(8080);
-console.log("SERVER ON PORT 8080")
+app.listen(process.env.PORT || 80);
+console.log("SERVER ON")
